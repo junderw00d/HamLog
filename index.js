91dd5d89d8c65d7af826e674e952c3bd3ba8bbf4
KoalaMuffin <jcu123123@gmail.com>
2018-02-24 10:43:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const {app} = require('electron')
 const {BrowserWindow} = require('electron')
 
+
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
     title:"HamLog",
     titleBarStyle: "hiddenInset",
   })
   mainWindow.loadURL('file://' + __dirname + '/app.html')
-})
+
+  /*
+  mainWindow.on("resize", function() {
+      mainWindow.webContents.send('message', mainWindow.getSize());
+  });
+  */
+
+});
