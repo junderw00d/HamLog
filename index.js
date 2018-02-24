7ea6836fe249bb47fc0634a569c42da1d8fca9ab
KoalaMuffin <jcu123123@gmail.com>
2018-02-24 16:13:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const {app} = require('electron')
-const {BrowserWindow} = require('electron')
+const {app} = require('electron');
+const {BrowserWindow} = require('electron');
+
 
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
     title:"HamLog",
     titleBarStyle: "hiddenInset",
-  })
-  mainWindow.loadURL('file://' + __dirname + '/app.html')
+  });
+  mainWindow.loadURL('file://' + __dirname + '/app.html');
 
   /*
   mainWindow.on("resize", function() {
@@ -16,3 +17,35 @@ app.on('ready', function() {
   */
 
 });
+
+const ipc = require('electron').ipcMain;
+const dialog = require('electron').dialog;
+
+ipc.on('save-dialog', function (event) {
+  const options = {
+    title:"Export HamLog Data",
+    buttonLabel:"Export",
+    defaultPath:app.getPath("desktop")+"/HamLog-data.json",
+    showsTagField: false,
+    filters: [
+      { name: 'JSON', extensions: ["json"] }
+    ]
+  };
+  dialog.showSaveDialog(options, function (filename) {
+    event.sender.send('saved-file', filename);
+  });
+});
+
+ipc.on('open-file-dialog', function (event) {
+const openOptions = {
+  properties: ['openFile'],
+  filters: [
+    { name: 'JSON', extensions: ["json"] }
+  ],
+buttonLabel:"Import",
+defaultPath:app.getPath("desktop")
+}
+  dialog.showOpenDialog(openOptions, function (filename) {
+    if (filename) event.sender.send('selected-directory', filename)
+  })
+})
